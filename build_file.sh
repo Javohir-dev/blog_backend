5cbcc3dc1e399c2026437c13ce0feb738f0e8b77
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-12-11 11:37:36 +0500

    Deploy

--- build_file.sh
+++ build_file.sh
@@ -1,7 +1,7 @@
-echo " BUILD START"
-python -m pip install pipenv
-python -m pipenv install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-python manage.py migrate
-python manage.py runserver
-echo " BUILD END"
+# echo " BUILD START"
+# python -m pip install pipenv
+# python -m pipenv install -r requirements.txt
+# python manage.py collectstatic --noinput --clear
+# python manage.py migrate
+# python manage.py runserver
+# echo " BUILD END"
